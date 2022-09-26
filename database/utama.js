a2138c58217f8ddfb8f310d36db3dbc56aee13f1
fajarmasri <fajaralam1604@gmai.com>
2022-09-26 13:41:43 +0800

    setting routes di utama js

--- database/utama.js
+++ database/utama.js
@@ -7,6 +7,10 @@ const port = 2002
 app.use(bodyParser.urlencoded({extended : true}))
 app.use(bodyParser.json())
 
+//panggil router
+var routes = require('/router.js')
+routes(app)
+
 app.listen(port, ()=>{
     console.log(`port ${port} dah oke...`)
 })
